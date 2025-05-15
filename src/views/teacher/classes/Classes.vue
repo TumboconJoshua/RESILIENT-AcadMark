e5b18fba7e6be51080a31daac6a1efbbbdb840a9
JoshuaRalphz <solomonjoshua016@gmail.com>
2025-05-16 03:39:42 +0800

    bug fix

--- src/views/teacher/classes/Classes.vue
+++ src/views/teacher/classes/Classes.vue
@@ -2,7 +2,7 @@
   <div>
     <h1 class="title">Classes</h1>
     <div class="grid ssm:grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-5">
-      <Card v-for="classItem in classes" :key="classItem.Class_ID" type="class" :trackStand="classItem.Track"
+      <Card v-for="classItem in classes" :key="classItem.Class_ID" type="class" :trackStand="getTrackForGrade(classItem.Track, classItem.Grade_Level)"
         :classType="classItem.classType" :className="classItem.ClassName" :subjectName="classItem.subject_name"
         :subject_id="classItem.subject_id" :gradeLevel="classItem.Grade_Level" />
     </div>
@@ -15,6 +15,11 @@ import Card from "@/components/card.vue";
 import ClassesService from "@/service/Classes/ClassesService";
 
 const classes = ref([]);
+
+const getTrackForGrade = (track, gradeLevel) => {
+  const grade = parseInt(gradeLevel);
+  return grade <= 10 ? 'Academic' : track;
+};
 
 onMounted(async () => {
   try {
